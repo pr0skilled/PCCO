20f380f2d4b99af723f2308a3e79b3f9c66cadb8
Alex <mahavoy2002@gmail.com>
2022-08-24 12:53:41 +0300

    paswword toggle visibility, styles upd

---
+++ PCCO/wwwroot/js/togglePassword.js
@@ -0,0 +1,24 @@
+﻿const togglePassword = document
+    .querySelector('#togglePassword');
+
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', () => {
+    const type = password
+        .getAttribute('type') === 'password' ?
+        'text' : 'password';
+
+    password.setAttribute('type', type);
+
+    if (togglePassword.classList.contains('fa-eye-slash')) {
+        togglePassword.classList.remove('fa-eye-slash');
+        togglePassword.classList.add('fa-eye');
+        return;
+    }
+
+    if (togglePassword.classList.contains('fa-eye')) {
+        togglePassword.classList.remove('fa-eye');
+        togglePassword.classList.add('fa-eye-slash');
+        return;
+    }
+});
